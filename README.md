d2c6cde7260f811a9163a7b47673d7790a337aca
inna-ianko <inna.yanko@gmail.com>
2022-03-07 14:11:57 +0200

    Added support of user API.

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Urban Jungle Application Overview
 
 Urban Jungle application provides ability to care for your home plants. 
+
+# API documentation
+
+## User APIs
+
+| HTTP Method | Resource URL| Notes | 
+| ---  | --- | ---|
+| GET  | /api/users/\<id> | Return a user. |
+| GET  | /api/users       | Return the collection of all user. |
+| POST | /api/users       | Register a new user account. |
+| PUT  | /api/users       | Return the collection of all users. |
+
+
+## API Authentication
+
+To receive an auth token please send request with basic authentication credentials:
+~~~
+http --auth <username>:<password> POST http://localhost:5000/api/tokens
+~~~
+
+For authentication of API request send "bearer" token using Authorization header. 
+~~~
+http GET http://localhost:5000/api/users/<id> "Authorization:Bearer <token>"
+~~~
+
+
+
+
+
+
+
+
+
+
--- app/models.py
+++ app/models.py
@@ -1,18 +1,46 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
+from flask import url_for
+import base64
+import os
 
 
-class User(UserMixin, db.Model):
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = query.paginate(page, per_page, False)
+        data = {
+            'items': [item.to_dict() for item in resources.items],
+            '_meta': {
+                'page': page,
+                'per_page': per_page,
+                'total_pages': resources.pages,
+                'total_items': resources.total
+            },
+            '_links': {
+                'self': url_for(endpoint, page=page, per_page=per_page,
+                                **kwargs),
+                'next': url_for(endpoint, page=page + 1, per_page=per_page,
+                                **kwargs) if resources.has_next else None,
+                'prev': url_for(endpoint, page=page - 1, per_page=per_page,
+                                **kwargs) if resources.has_prev else None
+            }
+        }
+        return data
+
+
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     plants = db.relationship('Plant', backref='owner', lazy='dynamic')
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -32,6 +60,48 @@ class User(UserMixin, db.Model):
         plants = Plant.query.filter_by(user_id=self.id)
         return plants.order_by(Plant.plant_name.desc())
 
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'about_me': self.about_me,
+            'plant_count': self.plants.count(),
+            '_links': {
+                'self': url_for('api.get_user', id=self.id),
+                'avatar': self.avatar(128)
+            }
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ['username', 'email', 'about_me']:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and 'password' in data:
+            self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
+
+
 
 @login.user_loader
 def load_user(id):
@@ -40,7 +110,7 @@ def load_user(id):
 
 class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    plant_name = db.Column(db.String(64), index=True, unique=True)
+    plant_name = db.Column(db.String(64), index=True)
     last_watering_timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
